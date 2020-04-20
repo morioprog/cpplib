ff21fa90fce80d8f9f2e178abce4c8ba89283fb7
morioprog <morio.prog@gmail.com>
2020-04-21 01:09:06 +0900

    add template/short.cpp

---
+++ template/short.cpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define REP(i, n)    for (ll (i) = 0;   (i) < (n); ++(i))
+#define FOR(i, a, b) for (ll (i) = (a); (i) < (b); ++(i))
---
+++ test/aoj/ITP1_1_A.short.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+
+#define call_from_test
+#include "../../template/short.cpp"
+#undef call_from_test
+
+signed main() {
+
+    cout << "Hello World" << endl;
+
+}
