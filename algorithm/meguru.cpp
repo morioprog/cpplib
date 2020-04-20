c30d7a32edd3b6d5a6bf174a18aadd1376a03b4b
morioprog <morio.prog@gmail.com>
2020-04-21 02:36:55 +0900

    add algorithm/meguru.cpp (IGNORE)

---
+++ algorithm/meguru.cpp
@@ -0,0 +1,11 @@
+#define IGNORE
+
+auto check = [&](long long mid) -> bool {
+    $3
+};
+
+long long ok = $1, ng = $2;
+while (abs(ok - ng) > 1) {
+    long long mid = (ok + ng) >> 1;
+    (check(mid) ? ok : ng) = mid;
+}
