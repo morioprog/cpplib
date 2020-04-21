e020f8c85a1605e7820f9cbf9c6a4b5192ad2f9f
morioprog <morio.prog@gmail.com>
2020-04-21 22:36:20 +0900

    add math/prime/divisor.cpp

---
+++ docs/math/prime/divisor.md
@@ -0,0 +1,14 @@
+## 概要
+
+約数を列挙する. *ソートはされていない*.
+
+## 計算量
+
+$O(\sqrt{n})$
+
+## 使用例
+
+```cpp
+int N = 105;
+vector<long long> div = divisor(N);
+```
---
+++ math/prime/divisor.hpp
@@ -0,0 +1,15 @@
+/**
+ * @brief Fraction (分数)
+ * @docs docs/math/prime/divisor.md
+ */
+
+vector<long long> divisor(const long long n) {
+    vector<long long> ret;
+    for (long long i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            ret.emplace_back(i);
+            if (i * i != n) ret.emplace_back(n / i);
+        }
+    }
+    return ret;
+}
