cfe3fa15429cb5256a632342a0cabc33c73a7af6
morioprog <morio.prog@gmail.com>
2020-04-21 14:07:07 +0900

    fix test

--- test/aoj/ITP1_1_A.short.test.cpp
+++ test/aoj/ITP1_1_A.short.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
 
 #define call_from_test
-#include "../../template/short.cpp"
+#include "../../template/short.hpp"
 #undef call_from_test
 
 signed main() {
--- test/aoj/ITP1_1_A.test.cpp
+++ test/aoj/ITP1_1_A.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
 
 #define call_from_test
-#include "../../template/main.cpp"
+#include "../../template/main.hpp"
 #undef call_from_test
 
 signed main() {
