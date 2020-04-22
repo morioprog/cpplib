2bc16c6e86ece4d9b8a2420504e5ed8d8b824157
morioprog <morio.prog@gmail.com>
2020-04-22 15:28:38 +0900

    add datastructure/sparsetable

---
+++ datastructure/sparsetable.hpp
@@ -0,0 +1,26 @@
+/**
+* @brief Sparse Table
+* @docs docs/datastructure/sparsetable.md
+*/
+
+template<typename T> struct SparseTable {
+    vector<vector<T>> st;
+    using F = function<T(T, T)>;
+    const F f;
+    SparseTable() {}
+    SparseTable(const vector<T> &v, const F f) : f(f) {
+        int b = 0, sz = v.size();
+        while ((1 << b) <= sz) ++b;
+        st.assign(b, vector<T>(1 << b));
+        for (int i = 0; i < sz; ++i) st[0][i] = v[i];
+        for (int i = 1; i < b; ++i) {
+            for (int j = 0; j + (1 << i) <= (1 << b); ++j) {
+                st[i][j] = f(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+            }
+        }
+    }
+    T query(const int l, const int r) const {
+        int b = 31 - __builtin_clz(r - l);
+        return f(st[b][l], st[b][r - (1 << b)]);
+    }
+};
---
+++ test/yosupo/staticrmq.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include "../../template/short.hpp"
+#include "../../datastructure/sparsetable.hpp"
+
+signed main() {
+
+    int N, Q;
+    cin >> N >> Q;
+
+    using T = long long;
+    vector<T> A(N);
+    for (auto& e: A) cin >> e;
+
+    SparseTable<T> st(A, [](T a, T b) { return min(a, b); });
+
+    while (Q--) {
+        int l, r;
+        cin >> l >> r;
+        cout << st.query(l, r) << endl;
+    }
+
+}
