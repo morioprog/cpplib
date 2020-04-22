ca19ffffcd3a7f5168aaabab067308cd325c19a9
morioprog <morio.prog@gmail.com>
2020-04-22 17:52:09 +0900

    add datastructure/cumulativesum/cumulativesum2d

---
+++ datastructure/cumulativesum/cumulativesum2d.hpp
@@ -0,0 +1,21 @@
+/**
+* @brief 2次元累積和
+* @docs docs/datastructure/cumulativesum/cumulativesum2d.md
+*/
+
+template<class T> struct CumulativeSum2D {
+    int H, W;
+    vector<vector<T>> data;
+    CumulativeSum2D(const vector<vector<T>> &v)
+        : H(v.size()), W(v[0].size()) {
+        data.assign(H + 1, vector<T>(W + 1, 0));
+        for (int i = 0; i < H; ++i) {
+            for (int j = 0; j < W; ++j) {
+                data[i + 1][j + 1] = v[i][j] + data[i + 1][j] + data[i][j + 1] - data[i][j];
+            }
+        }
+    }
+    T query(int sx, int sy, int gx, int gy) {
+        return data[gx][gy] - data[sx][gy] - data[gx][sy] + data[sx][sy];
+    }
+};
---
+++ test/atcoder/abc106_d.test.cpp
@@ -0,0 +1,28 @@
+#define IGNORE
+
+#define PROBLEM "https://atcoder.jp/contests/abc106/tasks/abc106_d"
+
+#include "../../template/main.hpp"
+#include "../../datastructure/cumulativesum/cumulativesum2d.hpp"
+
+signed main() {
+ 
+    int N, M, Q;
+    cin >> N >> M >> Q;
+ 
+    vector<vector<int>> v(N + 1, vector<int>(N + 1, 0));
+    while (M--) {
+        int L, R;
+        cin >> L >> R;
+        v[L][R]++;
+    }
+ 
+    CumulativeSum2D<int> acc(v);
+ 
+    while (Q--) {
+        int p, q;
+        cin >> p >> q;
+        print(acc.query(p, p, q + 1, q + 1));
+    }
+ 
+}
