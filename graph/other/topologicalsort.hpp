ab675202a9ab25ce21953e60bbfe82d46acd7906
morioprog <morio.prog@gmail.com>
2020-06-02 02:17:09 +0900

    add topologicalsort

---
+++ graph/other/topologicalsort.hpp
@@ -0,0 +1,22 @@
+/**
+* @brief トポロジカルソート
+* @docs docs/graph/other/topologicalsort.md
+*/
+
+template<typename T> vector<int> TopologicalSort(const Graph< T > &g) {
+    vector<int> order, color(g.V, 0);
+    auto rec = [&](auto &&f, int v) -> bool {
+        color[v] = 1;
+        for (auto& e: g.mat[v]) {
+            if (color[e] == 2) continue;
+            if (color[e] == 1) return false;
+            if (not f(f, e)) return false;
+        }
+        order.push_back(v);
+        color[v] = 2;
+        return true;
+    };
+    for (int i = 0; i < g.V; ++i) if (not color[i] and not rec(rec, i)) return vector<int>();
+    reverse(order.begin(), order.end());
+    return order;
+}
