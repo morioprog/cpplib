0e00379c8240144cd2815521ccb34b921a9bacf4
morioprog <morio.prog@gmail.com>
2020-07-06 11:11:16 +0900

    separate combinatorics

---
+++ math/combination.hpp
@@ -0,0 +1,68 @@
+/**
+ * @brief Combination
+ * @docs docs/math/combination.md
+ */
+
+template<typename T>
+struct Combination {
+    using u32 = unsigned int;
+    using i64 = long long;
+    Combination() {}
+    Combination(u32 n) { init(n); }
+    static vector<T> fact, finv, invs;
+    static void init(u32 n) {
+        u32 m = fact.size();
+        if (n < m) return;
+        fact.resize(n + 1, 1);
+        finv.resize(n + 1, 1);
+        invs.resize(n + 1, 1);
+        if (m == 0) m = 1;
+        for (u32 i = m; i <= n; ++i) fact[i] = fact[i - 1] * T(i);
+        finv[n] = T(1) / fact[n];
+        for (u32 i = n; i >= m; --i) finv[i - 1] = finv[i] * T(i);
+        for (u32 i = m; i <= n; ++i) invs[i] = finv[i] * fact[i - 1];
+    }
+    static T getFact(u32 n) { init(n); return fact[n]; }
+    static T getFinv(u32 n) { init(n); return finv[n]; }
+    static T getInvs(u32 n) { init(n); return invs[n]; }
+    static T C(i64 n, i64 r) {
+        if (r == 0) return T(1);
+        if (r <  0) return T(0);
+        if (n <  0) return T(r & 1 ? -1 : 1) * C(-n + r - 1, r);
+        if (n == 0 || n < r) return T(0);
+        init(n);
+        return fact[n] * finv[n - r] * finv[r];
+    }
+    static T P(i64 n, i64 r) {
+        if (n < r || r < 0) return T(0);
+        init(n);
+        return fact[n] * finv[n - r];
+    }
+    static T H(i64 n, i64 r) {
+        if (n < 0 || r < 0) return T(0);
+        if (!n && !r) return T(1);
+        init(n + r - 1);
+        return C(n + r - 1, r);
+    }
+    static T C_pascal(i64 n, i64 r) {
+        if (r == 0) return T(1);
+        if (r <  0) return T(0);
+        if (n <  0) return T(r & 1 ? -1 : 1) * C_pascal(-n + r - 1, r);
+        if (n == 0 || n < r) return T(0);
+        T ret = 1;
+        for (u32 i = 1; i <= r; ++i) ret *= n--, ret /= i;
+        return ret;
+    }
+    static T P_pascal(i64 n, i64 r) {
+        if (n < r || r < 0) return T(0);
+        T ret = 1;
+        for (u32 i = 1; i <= r; ++i) ret *= n--;
+        return ret;
+    }
+    static T H_pascal(i64 n, i64 r) {
+        if (n < 0 || r < 0) return T(0);
+        if (!n && !r) return T(1);
+        return C_pascal(n + r - 1, r);
+    }
+};
+
