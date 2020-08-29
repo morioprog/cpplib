58eeda040acebc96dec6ee451f53bd278ce67a13
Shota Iwamoto <45159150+morioprog@users.noreply.github.com>
2020-08-29 16:37:23 +0900

    Update generate_tex.py

--- generate_tex.py
+++ generate_tex.py
@@ -3,7 +3,7 @@ import yaml
 INPUT_YAML = r'lib.yml'
 OUTPUT_TEX = r'library.tex'
 HEADER = r'''\documentclass[a4paper,10pt,twocolumn,notitlepage]{article}
-\usepackage[top=10truemm,bottom=15truemm,left=10truemm,right=10truemm]{geometry}
+%\usepackage[top=10truemm,bottom=15truemm,left=10truemm,right=10truemm]{geometry}
 \usepackage{setspace}
 \usepackage{listings, jlisting}
 \usepackage{colortbl}
